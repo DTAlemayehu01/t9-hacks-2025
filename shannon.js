4ba8f23daa3aeb94dcaec4e90954ee917b918ad3
JerryFW <jewa3775@colorado.edu>
2025-02-15 00:34:16 -0700

    some shannon methods

---
+++ shannon.js
@@ -0,0 +1,37 @@
+function shannonEntropy(password) {
+    if (!password) return 0;
+    
+    let length = password.length;
+    let charCounts = {};
+    
+    // Count character frequencies
+    for (let char of password) {
+        charCounts[char] = (charCounts[char] || 0) + 1;
+    }
+    
+    // Compute entropy
+    let entropy = 0;
+    for (let char in charCounts) {
+        let p = charCounts[char] / length;
+        entropy -= p * Math.log2(p);
+    }
+    
+    return entropy;
+}
+
+function maxShannonEntropyofLengthN(n) {
+    let p = 1 / n; // uniform distribution for each character
+    let entropyPerChar = -p * Math.log2(p);
+    return length * entropyPerChar;
+}
+
+
+function shannonRatio(password)
+{
+    let length = password.length;
+    let entropy = shannonEntropy(password);
+    let theoreticalEntropy = maxShannonEntropyofLengthN(n);
+
+    let effectiveRatio = entropy/theoreticalEntropy
+    return effectiveRatio
+}
