6973273f3824942af25103debdaa694fcc9ce5be
JerryFW <jewa3775@colorado.edu>
2025-02-15 00:37:06 -0700

    Create passwordEntropy.js

---
+++ passwordEntropy.js
@@ -0,0 +1,35 @@
+
+//I personally think this one is a bit trivial but whatever
+function getPasswordCharSetSize(password) {
+    let charSets = {
+        lowercase: "abcdefghijklmnopqrstuvwxyz",
+        uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        numbers: "0123456789",
+        special: "!@#$%^&*()-_=+[]{}|;:\'\",.<>?/"
+    };
+    
+    let poolSize = 0;
+    let usedSets = new Set();
+    
+    for (let char of password) {
+        for (let [key, chars] of Object.entries(charSets)) {
+            if (chars.includes(char)) {
+                usedSets.add(key);
+            }
+        }
+    }
+    
+    for (let set of usedSets) {
+        poolSize += charSets[set].length;
+    }
+    
+    return poolSize;
+}
+
+function passwordEntropy(password) {
+    let length = password.length;
+    if (length === 0) return 0;
+    
+    let poolSize = getPasswordCharSetSize(password);
+    return length * Math.log2(poolSize);
+}
